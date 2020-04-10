84e86159c8caffa9ab4a212523560899e725c7e2
santidearribas <santiagodearribas@gmail.com>
2020-04-10 16:41:52 +0100

    Pairing with home assistant complete

--- __init__.py
+++ __init__.py
@@ -7,33 +7,33 @@ import os
 from os.path import join, exists
 import uuid
 
-def getserial():
-    # Extract serial from cpuinfo file
-    cpuserial = "0000000000000000"
-    try:
-        f = open('/proc/cpuinfo','r')
-        for line in f:
-            if line[0:6]=='Serial':
-                cpuserial = line[10:26]
-        f.close()
-    except:
-        cpuserial = "ERROR000000000"
-    return cpuserial
-
 class Daisy(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.serial_key = getserial()
-        self.user_id = None
-        self.username = None
+        self.serial_key = ""
         self.home_assistant_id = str(uuid.uuid4())[0:28]
+        self.user_id = ""
+        self.username = ""
 
         self.cred_file = join(self.root_dir, 'cred')
-  
-    @intent_handler(IntentBuilder('HiDaisy').require('Hi').require('Daisy'))
-    def handle_hi_daisy(self, Message):
+
+    def getserial(self):
+        # Extract serial from cpuinfo file
+        cpuserial = "0000000000000000"
+        try:
+            f = open('/proc/cpuinfo','r')
+            for line in f:
+                if line[0:6]=='Serial':
+                    cpuserial = line[10:26]
+            f.close()
+        except:
+            cpuserial = "ERROR000000000"
+        self.serial_key = cpuserial
+    
+    @intent_file_handler("hi.daisy.intent")
+    def handle_hi_daisy(self, message):
         self.check_cred()
-        if self.username == None:
+        if self.username is None:
             response = self.get_response("have you registered on the daisy app")
             if response == "yes":
                 code = self.get_response("whats your code")
@@ -85,8 +85,9 @@ class Daisy(MycroftSkill):
             return "ERROR"    
 
     def check_cred(self):
+        LOG.info(self.root_dir)
         if os.stat(self.cred_file).st_size == 0:
-            self.username = None
+            self.username is None
         else:
             with open(self.cred_file) as f:
                 username = f.read()
